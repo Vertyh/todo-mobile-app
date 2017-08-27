e6b1479356678450a6b206822f5e99209e2bdb34
robert.jedrzejewski <robert.jedrzejewski@dev-chop-chop.org>
2017-08-27 21:18:55 +0200

    Set folders basic structure

--- index.android.js
+++ index.android.js
@@ -1,54 +1,17 @@
 /**
- * Sample React Native App
+ * ToDoList React Native App
  * https://github.com/facebook/react-native
  * @flow
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { View, AppRegistry } from 'react-native';
+import App from './app/containers/App';
 
 export default class ToDoApp extends Component {
   render() {
-    console.log('test');
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+      return <App />
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('ToDoApp', () => ToDoApp);
